02478fe82c58b1b747c4452e3c57a1eff0db6b1e
Yarin Ben Shimon <313565202@idf.il>
2022-10-19 22:39:38 +0300

    fetch urls and get the first url

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { fetchImageUrls } from "../api/index";
+import { Typography } from '@mui/material';
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+    const [imageUrls, setImageUrls] = useState(null);
+    const [currentUrlToShow, setCurrentUrlToShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchUrls() {
+            const urls = await fetchImageUrls();
+            setImageUrls(urls);
+            if(urls.length > 0)
+                setCurrentUrlToShow(urls[0])
+            setIsLoading(false);
+        }
+
+        fetchUrls();
+    }, []);
+
+    if(isLoading){
+        // TODO: Loader.
+        return <Typography>loading</Typography>;
+    }
+
+    if(!isLoading && !imageUrls){
+        // TODO: error message.
+    }
+
+    return <h1>{currentUrlToShow}</h1>;
 };
 export default ImageCarousel;
