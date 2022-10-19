a35d40ca83b7d8713cc7e6e6c399661a00b001bf
Yarin Ben Shimon <313565202@idf.il>
2022-10-20 00:01:32 +0300

    fix test

--- src/task1/App.test.js
+++ src/task1/App.test.js
@@ -1,11 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from "./App";
 
 test("Testing works!", () => {
-  render(<App />);
-  expect(screen.getByTestId('App')).toBeInTheDocument();
-  /*const { getByText } = render(<App />);
+  const { getByText } = render(<App />);
   // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(getByText(/Your code/i)).toBeInTheDocument();*/
+  expect(getByText(/Your code/i)).toBeInTheDocument();
 });
 
